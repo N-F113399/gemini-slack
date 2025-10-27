00c95e4a9b7efceb3198b0be84cdf58f8e450750
N-F113399 <naoya1018589@gmail.com>
2025-10-27 14:26:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import slackEventsRouter from "./src/routes/slackEvents.js";
+import slackEventsRouter from "./routes/slackEvents.js";
 
 const app = express();
 app.use(express.json());
