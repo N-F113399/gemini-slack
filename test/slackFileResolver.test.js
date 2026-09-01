59ac25eecc97e6e13312aa61c7097372f72d9c67
N-F113399 <naoya1018589@gmail.com>
2026-09-01 16:44:55 +0900

    test: align Slack file assertions with content error codes

--- test/slackFileResolver.test.js
+++ test/slackFileResolver.test.js
@@ -1,7 +1,7 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 import { validateSlackFile } from "../src/services/content/slackFileResolver.js";
-import { ContentError } from "../src/services/content/contentErrors.js";
+import { ContentError, CONTENT_ERROR_CODES } from "../src/services/content/contentErrors.js";
 
 test("validateSlackFile accepts a valid Slack file", () => {
   assert.doesNotThrow(() => validateSlackFile({
@@ -15,7 +15,7 @@ test("validateSlackFile accepts a valid Slack file", () => {
 test("validateSlackFile rejects missing file id", () => {
   assert.throws(
     () => validateSlackFile({ url_private_download: "https://example.com/file" }),
-    (error) => error instanceof ContentError && error.code === "INVALID_FILE",
+    (error) => error instanceof ContentError && error.code === CONTENT_ERROR_CODES.INVALID_CONTENT,
   );
 });
 
@@ -30,7 +30,7 @@ test("validateSlackFile rejects a file over the configured size limit", () => {
         url_private_download: "https://files.slack.com/files-pri/F123/test.png",
         size: 101,
       }),
-      (error) => error instanceof ContentError && error.code === "FILE_TOO_LARGE",
+      (error) => error instanceof ContentError && error.code === CONTENT_ERROR_CODES.CONTENT_TOO_LARGE,
     );
   } finally {
     if (previous === undefined) delete process.env.MAX_SLACK_FILE_SIZE;
