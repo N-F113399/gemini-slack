4e5b38b943fdf025009d025705169232042b12f1
N-F113399 <naoya1018589@gmail.com>
2026-09-01 18:37:46 +0900

    feat: add search response parser boundaries

---
+++ src/services/search/responseParsers.js
@@ -0,0 +1,11 @@
+export function parseTavilyResponse(_response, _query) {
+  throw new Error("Tavily response parser is not implemented yet");
+}
+
+export function parseExaResponse(_response, _query) {
+  throw new Error("Exa response parser is not implemented yet");
+}
+
+export function parseYouResponse(_response, _query) {
+  throw new Error("You.com response parser is not implemented yet");
+}
